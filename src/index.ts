4b6450814684936fa12d1d4d9269147201b1299e
“MarikoOhtsuka” <mariko.ohtsuka@nnh.go.jp>
2021-08-11 11:55:04 +0900

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,18 @@
-function normalizeToNFC(targetObject: any) {
-/*  targetObject.forEach(target: any => {
-    if (target.getText() != '') {
-      target.setText(target.getText().normalize('NFC'));
-    }
-  });*/
-}
-function normalizeGoogleDocs() {
-  const targetDocsID = DocumentApp.getActiveDocument().getId();
-  const targetDocs = DocumentApp.openById(targetDocsID); 
-  const targetLists = [targetDocs.getBody().getParagraphs(), 
-                       targetDocs.getHeader().getParagraphs(), 
-                       targetDocs.getFooter().getParagraphs()]
-  targetLists.forEach(target => normalizeToNFC(target));
-}
-function inputDoc() {
-  const targetDocsUrl = 'https://docs.google.com/document/d/1iEMt9ZF5pSfGamWSs1urHprCFWr0XkmXWj6cGTaQZfk/edit';
-  const targetDocs = DocumentApp.openByUrl(targetDocsUrl);
-  const aaa = targetDocs.getBody().getParagraphs();
-  const targetTextList = aaa.map(x => x.getText());
-  const convertNFCTextList = checkNFC(targetTextList);
+function normalizeToNFC(target: any) {
+  const convertNFCTextList = checkNFC(target[1]);
   if (convertNFCTextList.length <= 0) {
     return;
   }
-  convertNFCTextList.forEach(x => targetDocs.getBody().replaceText(x[0], x[1]));
+  convertNFCTextList.forEach(x => target[0].replaceText(x[0], x[1])); 
+}
+function normalizeGoogleDocs() {
+  const targetDocsUrl = 'https://docs.google.com/document/d/1iEMt9ZF5pSfGamWSs1urHprCFWr0XkmXWj6cGTaQZfk/edit';
+  const targetDocs = DocumentApp.openByUrl(targetDocsUrl);
+  const targetLists = [targetDocs.getBody(), targetDocs.getHeader(), targetDocs.getFooter()].map(x => {
+    const textList = x.getParagraphs().map(x => x.getText());
+    return [x, textList];
+  });
+  targetLists.forEach(x => normalizeToNFC(x));
 }
 function checkNFC(target: string[]) {
     const checkStringArray = target.map(x => [x, convertNFC(x)]);
@@ -32,4 +22,3 @@ function convertNFC(target: string) {
     return target !== target.normalize('NFC') ? target.normalize('NFC') : '';
 }
 global.normalizeGoogleDocs = normalizeGoogleDocs
-global.inputDoc = inputDoc
