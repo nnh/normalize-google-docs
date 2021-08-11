f6378b3c2560101db257ecbff018fc02dab76dd4
“MarikoOhtsuka” <mariko.ohtsuka@nnh.go.jp>
2021-08-11 11:21:47 +0900

    temp

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,22 @@ function normalizeGoogleDocs() {
   targetLists.forEach(target => normalizeToNFC(target));
 }
 function inputDoc() {
-  const targetDocsUrl = 'https://docs.google.com/document/d/1A2F6TNh9lK3dSd04I5V-fpYi799xpg9F-qaGssGEefE/edit?usp=sharing';
+  const targetDocsUrl = 'https://docs.google.com/document/d/1iEMt9ZF5pSfGamWSs1urHprCFWr0XkmXWj6cGTaQZfk/edit';
   const targetDocs = DocumentApp.openByUrl(targetDocsUrl);
   const aaa = targetDocs.getBody().getParagraphs();
-  const bbb = aaa.map(x => x.getText());
-  console.log(bbb);
+  const targetTextList = aaa.map(x => x.getText());
+  const convertNFCTextList = checkNFC(targetTextList);
+  if (convertNFCTextList.length <= 0) {
+    return;
+  }
+  convertNFCTextList.forEach(x => targetDocs.getBody().replaceText(x[0], x[1]));
+}
+function checkNFC(target: string[]) {
+    const checkStringArray = target.map(x => [x, convertNFC(x)]);
+    return checkStringArray.filter(x => x[1] !== '');
+}
+function convertNFC(target: string) {
+    return target !== target.normalize('NFC') ? target.normalize('NFC') : '';
 }
 global.normalizeGoogleDocs = normalizeGoogleDocs
 global.inputDoc = inputDoc
